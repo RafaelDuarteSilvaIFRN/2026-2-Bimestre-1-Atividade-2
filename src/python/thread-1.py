282054d5ca3600ef934c78d572e734dc6d27302f
Leonardo Minora <leonardo.minora@gmail.com>
2026-08-31 08:40:00 -0300

    feat: adicionado código exemplo em python

---
+++ src/python/thread-1.py
@@ -0,0 +1,26 @@
+# thread - criar thread e empilhar para execução
+# 
+
+# Importa as libs padrões
+import threading
+import time
+
+# função que será executada na thread
+def minha_funcao():
+    print("Thread iniciada!")
+    time.sleep(2)
+    print("Thread finalizada!")
+
+def main();
+    # Criar a thread
+    thread = threading.Thread(target=minha_funcao)
+
+    # Iniciar a thread
+    thread.start()
+
+    # Aguardar a thread terminar
+    thread.join()
+    print("Programa principal finalizado!")
+
+if __name__ == "__main__":
+    main()
