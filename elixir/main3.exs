6cf0121aaf0137d4d809284b5235f177ac93dc97
hudy1717 <163691406+hudy1717@users.noreply.github.com>
2026-09-14 02:53:56 -0300

    Implement multi-threaded worker simulation

--- elixir/main3.exs
+++ elixir/main3.exs
@@ -1,1 +1,30 @@
+defmodule Projeto do
+  def trabalhador(numero, tempo_trabalho) do
+    IO.puts("Trabalhador #{numero} começou")
+    Process.sleep(tempo_trabalho * 1000)
+    IO.puts("Trabalhador #{numero} terminou (levou #{tempo_trabalho}s)")
+  end
 
+  def main do
+    IO.puts("Iniciando 5 trabalhadores...")
+    inicio = System.monotonic_time(:millisecond)
+
+    threads =
+      for i <- 0..4 do
+        Task.async(fn -> trabalhador(i, 2) end)
+      end
+
+    for thread <- threads do
+      Task.await(thread)
+    end
+
+    fim = System.monotonic_time(:millisecond)
+    tempo_total = (fim - inicio) / 1000.0
+
+    IO.puts("\nTodos os trabalhadores terminaram!")
+    IO.puts("Tempo total: #{:erlang.float_to_binary(tempo_total, decimals: 2)}s")
+    IO.puts("(Se fosse sequencial, levaria ~10s)")
+  end
+end
+
+Projeto.main()
